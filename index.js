8bd911e3180781be1bb86316938ac3e0b40f8d67
Roger Zhang <zhangrongjie01@dfwsgroup.com>
2022-05-13 16:41:35 +0800

    feat: 仅处理文件后缀为png和jpg的文件

--- index.js
+++ index.js
@@ -5,9 +5,12 @@ const files = fs.readdir(path.join(__dirname, 'img'), (err, data) => {
     // console.log(data, 'data')
     data.forEach((file,index) => {
         console.log(file)
-        fs.rename(`./img/${file}`, `./img/hello${index}.jpg`, (err) => {
-            if(err) throw err
-            console.log('Reame complete!')
-        })
+        const extname = path.extname(file)
+        if(['.jpg', '.png'].includes(extname)) {
+            fs.rename(`./img/${file}`, `./img/hello${index + extname}`, (err) => {
+                if(err) throw err
+                console.log('Reame complete!')
+            })
+        }
     });
 })
