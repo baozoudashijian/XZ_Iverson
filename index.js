0d9a3910d66b361cfed9b27589db8bf7079afd84
Roger Zhang <zhangrongjie01@dfwsgroup.com>
2022-05-13 17:23:32 +0800

    feat: 获取执行node的目录，而不是代码的目录

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const fs = require('fs')
 const path = require('path')
 
+console.log(__dirname, '=> dirname')
+console.log(process.cwd(), '=> cwd')
 function translate(prefix) {
-    fs.readdir(__dirname, (err, data) => {
+    fs.readdir(process.cwd(), (err, data) => {
         data.forEach((file,index) => {
+            console.log(file, '=> file')
             const extname = path.extname(file)
             if(['.jpg', '.png'].includes(extname)) {
                 let newFileName = prefix + index + extname
-                fs.rename(`${path.join(__dirname, file)}`, `${path.join(__dirname, newFileName)}`, (err) => {
+                fs.rename(`${path.join(process.cwd(), file)}`, `${path.join(process.cwd(), newFileName)}`, (err) => {
                     if(err) throw err
                     console.log('Reame complete!')
                 })
