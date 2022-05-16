43b95e7439e6587ee98e544bfc172bb7a90e1110
Roger Zhang <1787009521@qq.com>
2022-05-16 21:24:23 +0800

    feat: 复制文件过来，放文件过去

--- cli.js
+++ cli.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 const { Command } = require('commander')
 const program = new Command();
-const { translate } = require('./index')
+const { translate, initTemplate, rebaseTemplate } = require('./index')
 
 program
     .name('xz-tools')
@@ -13,6 +13,23 @@ program
     .action((str) => {
         // 控制台获取需要翻译的内容
         translate(str)
+    });
+
+// 使用模版初始化一个任务
+program.command('init')
+    .description('Init peoject by template')
+    .argument('<string>', 'Template name')
+    .argument('<string>', 'Project name')
+    .action((proNo, tempNo) => {
+        initTemplate(proNo, tempNo)
+    });
+
+program.command('rebase')
+    .description('Remove file rebase')
+    .argument('<string>', 'Template name')
+    .argument('<string>', 'Project name')
+    .action((proNo, tempNo) => {
+        rebaseTemplate(proNo, tempNo)
     });
 
 program.parse();
--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const fs = require('fs')
+const fse = require('fs-extra')
 const path = require('path')
 
 console.log(__dirname, '=> dirname')
 console.log(process.cwd(), '=> cwd')
 function translate(prefix) {
     fs.readdir(process.cwd(), (err, data) => {
-        data.forEach((file,index) => {
-            console.log(file, '=> file')
+        data.forEach((file, index) => {
             const extname = path.extname(file)
-            if(['.jpg', '.png'].includes(extname)) {
+            if (['.jpg', '.png'].includes(extname)) {
                 let newFileName = prefix + index + extname
                 fs.rename(`${path.join(process.cwd(), file)}`, `${path.join(process.cwd(), newFileName)}`, (err) => {
-                    if(err) throw err
+                    if (err) throw err
                     console.log('Reame complete!')
                 })
             }
@@ -19,7 +19,77 @@ function translate(prefix) {
     })
 }
 
+function initTemplate(proName, dirName) {
+    const ppath = path.join(process.cwd(), proName)
+    // 文件复制文件
+    const htmlSrc = path.join(process.cwd(), dirName, 'index.html')
+    const htmlDest = path.join(process.cwd(), proName, 'index.html')
+    // 文件夹复制文件夹
+    const src = path.join(process.cwd(), '..', 'lms-html-static', dirName)
+    const dest = path.join(process.cwd(), proName)
+
+    // 创建一个目录
+    console.log(proName, dirName)
+    if (!(fs.existsSync(ppath))) {
+        fs.mkdir(ppath, (err) => {
+            if (err) return
+            console.log('创建目录成功')
+            copyFile(src, dest)
+            copyFile(htmlSrc, htmlDest)
+        })
+    } else {
+        copyFile(src, dest)
+        copyFile(htmlSrc, htmlDest)
+    }
+
+}
+
+function rebaseTemplate(proName, dirName) {
+    const ppath = path.join(process.cwd(), '..', 'lms-html-static', proName)
+    const proSrc = path.join(process.cwd(), proName)
+    // 文件复制文件
+    // const htmlSrc = path.join(process.cwd(), dirName, 'index.html')
+    // const htmlDest = path.join(process.cwd(), proName, 'index.html')
+
+    // 创建一个目录
+    console.log(proName, dirName)
+    if (!(fs.existsSync(ppath))) {
+        fs.mkdir(ppath, (err) => {
+            if (err) return
+            console.log('创建目录成功')
+            rebaseFile(proSrc, proName)
+        })
+    } else {
+        rebaseFile(proSrc, proName)
+    }
+
+}
+
+function copyFile(src, dest) {
+    fse.copy(src, dest, err => {
+        if (err) return console.error(err)
+        console.log('success!')
+    })
+}
+
+function rebaseFile(proSrc, proName) {
+    fs.readdir(proSrc, (err, data) => {
+        if (err) {
+            console.log(err)
+        }
+        data.map((item) => {
+            if (!item.endsWith('.html')) {
+                const src = path.join(process.cwd(), proName, item)
+                const desc = path.join(process.cwd(), '..', 'lms-html-static', proName, item)
+                copyFile(src, desc)
+            }
+        })
+    })
+}
+
 module.exports = {
-    translate
+    translate,
+    initTemplate,
+    rebaseTemplate
 }
 
